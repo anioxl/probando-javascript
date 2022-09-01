c968192efe9ae3245440e05d57810fd085c44667
anioxl <analia.oxley@gmail.com>
2022-09-01 12:53:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# script-typemodule-srcprobando-jsscript
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-gpvcsi)
---
+++ probando.js
@@ -0,0 +1,5 @@
+console.log('Hello!');
+console.error("se ve en la consola como un mensaje de error")
+document.getElementById("mensaje").innerText = "Este es un parrafo desde javascript"
+
+
